38714dbdffc62ad82ef205e16251b69f07e04918
turtleman270 <ahill5067@gmail.com>
2016-11-06 09:06:19 -0600

    Create doctor_login.php

---
+++ AskMe-master/doctor_login.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    header("Content-Type: application/json");
+    ini_set("session.cookie_httponly", 1);
+ 
+    require 'database.php';
+     
+    // Use a prepared statement
+    $stmt = $mysqli->prepare("SELECT COUNT(*), id, password FROM doctors WHERE id=?");   
+    // Bind the parameter
+    $stmt->bind_param('s', $id);
+    $id = $_POST['id'];
+    // Execute
+    $stmt->execute();
+    // Bind and fetch the results
+    $stmt->bind_result($cnt, $id, $pwd_hash);
+    $stmt->fetch();
+     
+    $pwd_guess = $_POST['password'];
+    // Compare the submitted password to the actual password hash
+    if ($cnt == 1 && crypt($pwd_guess, $pwd_hash)==$pwd_hash) {
+        // Login succeeded!
+        $_SESSION['doctor_id'] = $id;
+        $_SESSION['token'] = substr(md5(rand()), 0, 10); // generate a 10-character random string
+        // Redirect to news page
+        echo json_encode(array(
+            "success" => true,
+            "token" => $_SESSION['token'],
+            "id" => $id
+        ));
+        exit;
+    } else {
+        echo json_encode(array(
+            "success" => false,
+            "message" => "Incorrect DoctorId or Password"
+        ));
+        exit;
+    }
+?>
